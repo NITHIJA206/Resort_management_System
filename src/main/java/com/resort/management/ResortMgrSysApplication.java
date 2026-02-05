dcc49bac4e920b1bd1472206cc4f5a92660aebcd
NITHIJA <niksri2006@gmail.com>
2026-02-05 22:46:37 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/resort/management/ResortMgrSysApplication.java
@@ -0,0 +1,13 @@
+package com.resort.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortMgrSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResortMgrSysApplication.class, args);
+	}
+
+}
